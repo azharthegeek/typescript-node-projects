df239c25fb00da8129f1ef27be6c6cc379c9c406
Muhammad Azhar <azharthegeek@gmail.com>
2023-02-12 17:16:24 +0500

    Project 00 Complted

---
+++ project00_calculator/index.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+async function main() {
+    const answers = await inquirer.prompt([
+        {
+            type: "number",
+            name: "NumberOne",
+            message: "Enter a 1st number",
+        },
+        {
+            type: "number",
+            name: "NumberTwo",
+            message: "Enter a 2nd number",
+        },
+        {
+            type: "list",
+            name: "Operator",
+            choices: ["Add", "Subtract", "Multiply", "Divide"],
+            message: "Select Operator"
+        }
+    ]);
+    const { NumberOne, NumberTwo, Operator } = answers;
+    switch (Operator) {
+        case "Add":
+            console.log("Result of ", Operator, " of ", NumberOne, " and ", NumberTwo, " is ", NumberOne + NumberTwo);
+            break;
+        case "Subtract":
+            console.log("Result of ", Operator, " of ", NumberOne, " and ", NumberTwo, " is ", NumberOne - NumberTwo);
+            break;
+        case "Multiply":
+            console.log("Result of ", Operator, " of ", NumberOne, " and ", NumberTwo, " is ", NumberOne * NumberTwo);
+            break;
+        case "Divide":
+            console.log("Result of ", Operator, " of ", NumberOne, " and ", NumberTwo, " is ", NumberOne / NumberTwo);
+            break;
+        default:
+            console.log("Invalid Input Please Try Again");
+            break;
+    }
+}
+main();
